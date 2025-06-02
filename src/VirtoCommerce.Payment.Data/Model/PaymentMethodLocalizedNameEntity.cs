b565439303ad273cae0fa82481b3cc59743c7806
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-06-02 16:56:58 +0200

    fix: remove unnecessary class, comments

--- src/VirtoCommerce.Payment.Data/Model/PaymentMethodLocalizedNameEntity.cs
+++ src/VirtoCommerce.Payment.Data/Model/PaymentMethodLocalizedNameEntity.cs
@@ -7,11 +7,11 @@ public class PaymentMethodLocalizedNameEntity : Entity
 {
     [Required]
     [StringLength(16)]
-    public string LanguageCode { get; set; } = string.Empty; // e.g., "en-US"
+    public string LanguageCode { get; set; } = string.Empty;
 
     [Required]
     public string Value { get; set; } = string.Empty;
 
-    public string ParentEntityId { get; set; } // Foreign key to the parent entity
+    public string ParentEntityId { get; set; }
     public virtual StorePaymentMethodEntity ParentEntity { get; set; } = null!;
 }
