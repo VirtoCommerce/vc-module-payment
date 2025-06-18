c8e8485a226048e89822ab32c11d8ca3c6838aa4
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-06-18 13:07:10 +0200

    feat: add missing migrations

--- src/VirtoCommerce.PaymentModule.Data.MySql/Migrations/20250618110504_AddPaymentMethodLocalizedName.Designer.cs
+++ src/VirtoCommerce.PaymentModule.Data.MySql/Migrations/20250618110504_AddPaymentMethodLocalizedName.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.PaymentModule.Data.Repositories;
 
@@ -10,9 +11,11 @@ using VirtoCommerce.PaymentModule.Data.Repositories;
 namespace VirtoCommerce.PaymentModule.Data.MySql.Migrations
 {
     [DbContext(typeof(PaymentDbContext))]
-    partial class PaymentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250618110504_AddPaymentMethodLocalizedName")]
+    partial class AddPaymentMethodLocalizedName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
